87669e5eecb4128080afeda75092e7b35ab7ce86
Juan Filippo <juanfilippo0@gmail.com>
2017-03-28 17:40:08 -0300

    v1

---
+++ app/src/main/java/com/example/athena/emporiomapp/CadastroActivity.java
@@ -0,0 +1,21 @@
+package com.example.athena.emporiomapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CadastroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void voltar(View v){
+        Intent intentv = new Intent(this,MainActivity.class);
+        startActivity(intentv);
+    }
+}
--- app/src/main/java/com/example/athena/emporiomapp/MainActivity.java
+++ app/src/main/java/com/example/athena/emporiomapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.athena.emporiomapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cadastrarLayout(View v){
+        Intent intent = new Intent(this,CadastroActivity.class);
+        startActivity(intent);
+    }
+
+    public void entrar(View v){
+        Intent intent = new Intent(this,MenuActivity.class);
+        startActivity(intent);
+    }
+
 }
