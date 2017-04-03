9aaff5f8cd0ff6ba3fc6b8b2cfb410c039c560b6
Juan Filippo <juanfilippo0@gmail.com>
2017-04-03 00:58:57 -0300

    v1.2 Correcao de erro

--- app/src/main/java/com/example/athena/emporiomapp/MainActivity.java
+++ app/src/main/java/com/example/athena/emporiomapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     Usuario usuario;
@@ -22,14 +21,14 @@ public class MainActivity extends Activity {
     }
 
     public void cadastrarLayout(View v) {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
+        /*if (usuario.getSenha().toString() == editSenha.getText().toString()) {
 
-        if (usuario.getSenha().toString() == editSenha.getText().toString()) {
-            Intent intent = new Intent(this, CadastroActivity.class);
-            startActivity(intent);
         } else {
             Toast.makeText(MainActivity.this,
                     "Senha Errada.", Toast.LENGTH_SHORT).show();
-        }
+        }*/
     }
 
     public void entrar(View v) {
